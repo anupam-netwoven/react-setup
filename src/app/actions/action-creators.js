9d3da7f9b673668c029ea7f57a0c7b3a1dee1a0f
Anupam Bhattacharyya <anupam@Anupams-MacBook-Air.local>
2018-03-16 12:56:54 +0530

    update

--- src/app/actions/action-creators.js
+++ src/app/actions/action-creators.js
@@ -1,7 +1,19 @@
-export const addUser = (payload) => {
-    return { type: 'ADD_USER', payload: payload }
-}
+// export const addUser = (payload) => {
+//     setTimeout(() => {
+//         return { type: 'ADD_USER', payload: payload }
+//     }, 2000)
+//     return { type: 'ADD_USER_TEMP', payload: payload }
+// }
 
 export const addProduct = (payload) => {
     return { type: 'ADD_PRODUCT', payload: payload }
+}
+
+export const addUser = (payload) => {
+    return (dispatch) => {
+        dispatch({ type: 'ADD_USER_IN_PROGRESS', payload: payload })
+        setTimeout(() => {
+            dispatch({ type: 'ADD_USER', payload: payload })
+        }, 3000)
+    };
 }
--- src/app/containers/app-container.js
+++ src/app/containers/app-container.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
-//import { bindActionCreators } from "redux";
-//import * as actionCreators from "../actions/action-creators";
+import { bindActionCreators } from "redux";
+import * as actionCreators from "../actions/action-creators";
 import { LayoutContainer } from "./layout-container";
 
 
@@ -11,20 +11,24 @@ function mapStateToProps(state) {
     }
 }
 
-// const mapDispatchToProps = (dispatch) => {
-//     return bindActionCreators(actionCreators, dispatch);
-// }
-
-function mapDispatchToProps(dispatch) {
-    return {
-        addUser: (payload) => {
-            dispatch({ type: 'ADD_USER', payload: payload })
-        },
-        addProduct: (payload) => {
-            dispatch({ type: 'ADD_PRODUCT', payload: payload })
-        }
-    }
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators(actionCreators, dispatch);
 }
 
-export const AppContainer = connect(mapStateToProps, mapDispatchToProps)(LayoutContainer);
+// function mapDispatchToProps(dispatch) {
+//     return {
+//         addUser: (payload) => {
+//             //dispatch({ type: 'ADD_USER', payload: payload })
+//             setTimeout(() => {
+//                 dispatch({ type: 'ADD_USER', payload: payload })
+//             }, 4000);
+//         },
+//         addProduct: (payload) => {
+//             dispatch({ type: 'ADD_PRODUCT', payload: payload })
+//         }
+//     }
+// }
+
+export const AppContainer =
+    connect(mapStateToProps, mapDispatchToProps)(LayoutContainer);
 
